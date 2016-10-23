d109e8b4dda0f4138bc7778bfca3c4393e376f6f
olezhek85 <olezhek_85@list.ru>
2016-10-23 15:17:11 +0300

    Ingredients List with *ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,9 +6,11 @@ import { Recipe } from '../recipe'
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'https://bigoven-res.cloudinary.com/image/upload/main---wiener-schnitzel-8f03d26e1f0f2601215a7029.jpg', []),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Dummy', 'http://thumbs1.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg');
 
   constructor() {
   }
--- src/app/recipes/recipe.ts
+++ src/app/recipes/recipe.ts
@@ -1,5 +1,5 @@
-export class Recipe {
-  constructor(public name: string, public description: string, public imagePath: string) {
+import { Ingredient } from '../shared/ingredient'
 
-  }
+export class Recipe {
+  constructor(public name: string, public description: string, public imagePath: string, public ingredients: Ingredient[]) {  }
 }
