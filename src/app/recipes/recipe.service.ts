8c6b900e2836f4479e8e5d517c7f243dfab86ff7
olezhek85 <olezhek_85@list.ru>
2016-10-27 19:15:07 +0300

    Cross-Service Communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from "./recipe";
+import {Ingredient} from "../shared/ingredient";
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'https://bigoven-res.cloudinary.com/image/upload/main---wiener-schnitzel-8f03d26e1f0f2601215a7029.jpg', []),
+    new Recipe('Schnitzel', 'Very tasty', 'https://bigoven-res.cloudinary.com/image/upload/main---wiener-schnitzel-8f03d26e1f0f2601215a7029.jpg', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Pork meat', 1)
+    ]),
     new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
   ];
 
