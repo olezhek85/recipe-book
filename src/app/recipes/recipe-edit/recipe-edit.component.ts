d864c64a73147dd1a94c61a083e07346bad0be4b
olezhek85 <olezhek_85@list.ru>
2016-11-03 21:13:15 +0200

    Creating the Recipe Form with the Data-Driven Approach

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,30 +1,44 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {
+  FormArray,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+}
+  from "@angular/forms";
 
 import {RecipeService} from "../recipe.service";
 import {Subscription} from "rxjs";
+import {Recipe} from "../recipe";
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html'
 })
 export class RecipeEditComponent implements OnInit, OnDestroy {
+  recipeForm: FormGroup;
   private recipeIndex: number;
+  private recipe: Recipe;
+  private isNew = true;
   private subscription: Subscription;
 
   constructor(private route: ActivatedRoute,
-              private recipeService: RecipeService) {
+              private recipeService: RecipeService,
+              private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
-    let isNew = true;
     this.subscription = this.route.params.subscribe(
       (params: any) => {
         if (params.hasOwnProperty('id')) {
+          this.isNew = false;
           this.recipeIndex = +params['id'];
-          isNew = false;
+          this.recipe = this.recipeService.getRecipe(this.recipeIndex);
         } else {
-          isNew = true;
+          this.isNew = true;
+          this.recipe = null;
         }
       }
     );
@@ -34,4 +48,36 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
+  private initForm(isNew: boolean) {
+    let recipeName = '';
+    let recipeImageUrl = '';
+    let recipeContent = '';
+    let recipeIngredients: FormArray = new FormArray([]);
+
+    if (!this.isNew) {
+      for (let i = 0; i < this.recipe.ingredients.length; i++) {
+        recipeIngredients.push(
+          new FormGroup(
+            {
+              name: new FormControl(this.recipe.ingredients[i].name, Validators.required),
+              amount: new FormControl(this.recipe.ingredients[i].amount, [
+                Validators.required,
+                Validators.pattern("\\d+")
+              ])
+            }
+          )
+        );
+      }
+      recipeName = this.recipe.name;
+      recipeImageUrl = this.recipe.imagePath;
+      recipeContent = this.recipe.description;
+    }
+
+    this.recipeForm = this.formBuilder.group({
+      name: [recipeName, Validators.required],
+      imagePath: [recipeImageUrl, Validators.required],
+      description: [recipeContent, Validators.required],
+      ingredients: recipeIngredients
+    });
+  }
 }
